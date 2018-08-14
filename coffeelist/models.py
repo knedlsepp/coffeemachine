cd87fb3c783f391b5c48e267b5d0ed7fdaa1b255
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-08-14 19:51:37 +0200

    Compute the balance

--- coffeelist/models.py
+++ coffeelist/models.py
@@ -1,27 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
+
 
 class Tag(models.Model):
     tag_value = models.CharField(max_length=200, unique=True)
     owner = models.ForeignKey(User, null=True, blank=True)
+
     def __str__(self):
         return '({}, Owner:{})'.format(self.tag_value, self.owner)
 
-class Purchase(models.Model):
-    tag = models.ForeignKey(Tag, null=False)
-    date = models.DateTimeField('date of purchase')
-    def __str__(self):
-        return '(Date: {:%x %X}, Tag:{})'.format(self.date, self.tag)
 
 class Price(models.Model):
-    date = models.DateTimeField('Date when the price changed')
+    date = models.DateTimeField('Date when the price changed', default=now)
     euros = models.DecimalField(max_digits=6, default=0, decimal_places=2)
+
     def __str__(self):
-        return '(Date: {}, Value: {}€)'.format(self.date, self.euros)
+        return '({}€ since {:%x})'.format(self.euros, self.date)
+
+
+class Purchase(models.Model):
+    tag = models.ForeignKey(Tag, null=False)
+    date = models.DateTimeField(
+        'date of purchase', default=now)  # Probably incorrect without a RTC
+    price = models.ForeignKey(Price, null=False)
+
+    def __str__(self):
+        return '(Date: {:%x %X}, Tag:{}, Price: {})'.format(
+            self.date, self.tag, self.price)
+
 
 class Deposit(models.Model):
     person = models.ForeignKey(User)
-    date = models.DateTimeField('Date of transaction')
+    date = models.DateTimeField('Date of transaction', default=now)
     euros = models.DecimalField(max_digits=6, decimal_places=2)
+
     def __str__(self):
-        return '({person}, {date:%x %X}, {euros}€)'.format(person=self.person, date=self.date, euros=self.euros)
+        return '({person}, {date:%x %X}, {euros}€)'.format(
+            person=self.person, date=self.date, euros=self.euros)
--- coffeelist/views.py
+++ coffeelist/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from coffeelist.models import *
+from django.db.models import Avg, Sum, Count
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse
+
+import pandas as pd
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the coffeelist index. Bla.")
+
+    user_deposit = {
+        user.id: user.deposits
+        for user in User.objects.annotate(deposits=Sum('deposit__euros'))
+    }
+    response = [{
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'balance': user_deposit[user.id] - user.expenses
+    } for user in User.objects.annotate(
+        expenses=Sum('tag__purchase__price__euros'))]
+    return JsonResponse(response, safe=False)

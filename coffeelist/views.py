e16949a9bb9da93c3dd39f42a2ea9132ecb647e9
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-08-15 08:40:21 +0200

    Set default values for totals to 0.

--- coffeelist/views.py
+++ coffeelist/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from coffeelist.models import *
-from django.db.models import Avg, Sum, Count
+from django.db.models import Avg, Sum, Count, Value
+from django.db.models.functions import Coalesce
 from django.contrib.auth.models import User
 import json
 from django.http import JsonResponse
@@ -13,7 +14,8 @@ def index(request):
 
     user_deposit = {
         user.pk: user.deposits
-        for user in User.objects.annotate(deposits=Sum('deposit__euros'))
+        for user in User.objects.annotate(
+            deposits=Coalesce(Sum('deposit__euros'), Value(0.)))
     }
     response = [{
         'pk': user.pk,
@@ -23,13 +25,16 @@ def index(request):
         'total_purchases': user.total_purchases,
         'balance': user_deposit[user.pk] - user.total_purchases,
     } for user in User.objects.annotate(
-        total_purchases=Sum('tag__purchase__price__euros'))]
+        total_purchases=Coalesce(
+            Sum('tag__purchase__price__euros'), Value(0.)))]
 
     df = pd.DataFrame(response)
     df.set_index('pk', inplace=True)
     print(df)
     response = df[[
-        'full_name', 'balance',
-        'total_deposits', 'total_purchases'
+        'full_name',
+        'total_deposits',
+        'total_purchases',
+        'balance',
     ]].to_html()
     return HttpResponse(response)

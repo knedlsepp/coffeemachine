c5e7d2acd450887ed05dbd8de7f9b4dd234edf57
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-08-15 09:44:33 +0200

    Package project using setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from glob import glob
+setup(
+    name='coffeemachine',
+    version='1.0',
+    scripts=glob('scripts/*'),
+    packages=find_packages(),
+    setup_requires=['django', 'pyscard', 'pandas'])
